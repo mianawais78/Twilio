7a2fbcd417fff0a9e7fbc582aa8fa682dc3676c2
HM Awais Hassan <109977640+mianawais78@users.noreply.github.com>
2023-03-18 13:16:39 +0500

    day3

---
+++ frontend/src/hooks/useLocalStorage.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+  const setValue = (value) => {
+    try {
+      localStorage.setItem(key, JSON.stringify(value));
+      setStoredValue(value);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return [setStoredValue, setValue];
+};
+
+export default useLocalStorage;
